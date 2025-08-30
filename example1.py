b85a65a1db6fcd66daded7bf5b02c1762f942b0a
PRANAV77-PY <pbse1377@gmail.com>
2025-08-30 22:05:50 +0530

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,16 @@
+#split array in 3 parts
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+
+newarr = np.array_split(arr,3)
+
+print(newarr)
+
+# split in array 4
+
+arr = np.array([1,2,3,4,5,6,])
+
+newarr = np.array_split(arr,4)
+
+print(newarr)
